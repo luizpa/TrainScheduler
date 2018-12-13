f7ab6a374651e83c9ca923062d830b3dd582cac3
Coding <coding@Paulo.attlocal.net>
2018-12-13 16:51:54 -0500

    Final Product

---
+++ assets/javascript/scheduler.js
@@ -0,0 +1,127 @@
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyA4rddjhXPZ4UbPczlnwLR08tq8F0qbikY",
+    authDomain: "train-scheduler-f51a7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f51a7.firebaseio.com",
+    projectId: "train-scheduler-f51a7",
+    storageBucket: "train-scheduler-f51a7.appspot.com",
+    messagingSenderId: "576140687662"
+  };
+  
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  };
+  
+  var database = firebase.database();
+  var trainName = "";
+  var destination = "";
+  var frequency = 0;
+  var firstTime = "";
+  var newTrain = {};
+  var temperature ='';
+  var weather ='';
+  
+  // 2. Button for adding Train Schedule
+  $("#add-train").on("click", function (event) {
+    event.preventDefault();
+  
+    //Grabs user input
+    trainName = $("#trainName-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    frequency = $("#frequency-input").val();
+    // moment().format('MMMM Do YYYY, h:mm:ss a');
+    firstTime = $("#firstTime-input").val();
+    // console.log(firstTime);
+  
+  
+  
+    // Creates local "temporary" object for holding train information data
+    newTrain = {
+      train: trainName,
+      destination: destination,
+      frequency: frequency,
+      firstTime: firstTime
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain);
+    // console.log(newTrain.destination);
+    // console.log(newTrain.frequency);
+    // console.log(newTrain.firstTime);
+  
+  
+    // Clears all of the text-boxes
+    $("#trainName-input").val("");
+    $("#destination-input").val("");
+    $("#frequency-input").val("");
+    $("#firstTime-input").val("");
+  
+  });
+  
+  // 3. Create Firebase event for adding a row in the html when a user adds an entry
+  // Since multiple listeners may be attached the firebase "on' event, on page load or reauthentication etc.
+  // We must first remove the existing listener before attaching new listener.
+  // In firebase call off() event before on() event to fix this.
+  database.ref().off();
+  database.ref().on("child_added", function (snap) {
+    console.log(snap.val());
+  
+    // Store everything into a variable.
+    trainName = snap.val().train;
+    destination = snap.val().destination;
+    frequency = snap.val().frequency;
+    firstTime = snap.val().firstTime;
+    // console.log(trainName);
+  
+  
+    // Prettify the time of the first train
+    var firstTimePretty = moment.unix(firstTime).format("hh:mm");
+  
+    // // Calculate the next arrival time and minutes away
+    // // Calculate the minutes away
+    var minAway = frequency - ((moment().diff(moment(firstTime, "X"), "minutes")) % frequency)
+    // console.log(minAway);
+  
+  
+    var nextArr = moment().add(minAway, "minutes").format('LT');
+    // console.log(nextArr);
+    // Get weather information for the destination city
+  
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + destination + "&units=imperial&APPID=d113a487796be0908add1c04f29fec71";
+  
+    var apiKey = "08d43ec37b557131abd63c112efe0b4b"
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+      temperature = response.main.temp;
+      weather = response.weather[0].description;
+      console.log(temperature);
+      console.log(weather);
+  
+   });
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").html(trainName),
+      $("<td>").html(destination),
+      $("<td>").html(frequency),
+      $("<td>").html(nextArr),
+      $("<td>").html(minAway)
+      // $("<td>").html(temperature + "&#8457 and " + weather)
+    );
+  
+  
+    // Append the new row to the table
+    console.log(temperature);
+    $("#train-schedule").append(newRow);
+   
+
+
+
+});
